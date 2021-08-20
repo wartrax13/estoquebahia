9ee50a94c2f927481d1dc5b682a687bc702e5231
wartrax13 <pedro.moisesandrade@gmail.com>
2021-08-20 19:44:30 -0300

    Criado lista de produtos close #9

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Produto
 
 
-
+def produto_list(request):
+    template_name = 'produto_list.html'
+    objects = Produto.objects.all()
+    context = {'object_list': objects}
+    return render(request, template_name, context)
 # Create your views here.
